be57450704d5ff8018d662a27b57d630f5ee4a18
itibara99 <itibara99@gmail.com>
2026-02-02 15:48:06 +0300

    Updated HeroSection.tsx

--- frontend/src/components/ceiling/HeroSection.tsx
+++ frontend/src/components/ceiling/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection: React.FC = () => {
       title: "ДЛЯ НАТЯЖНЫХ ПОТОЛКОВ",
       subtitle: "Алюминиевые профили",
       description: "Высококачественные карнизы, багеты, стеновые, комплектующие и т.д.",
-      image: ceilinghero.jpg,
+      image: "frontend/dist/assets/ceilinghero.jpg",
     },
     {
       title: "ВЕСЬ ПРОФИЛЬ",
