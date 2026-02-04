bce8a059802af9e3c5bd40e6ada7ebb9968d92b4
itibara99 <itibara99@gmail.com>
2026-02-04 15:22:21 +0300

    Updated HeroSection.tsx

--- frontend/src/components/ceiling/HeroSection.tsx
+++ frontend/src/components/ceiling/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection: React.FC = () => {
       subtitle: "Алюминиевые профили",
       description: "Высококачественные карнизы, багеты, стеновые, комплектующие и т.д.",
       image: ceilingSlide,
-      video: "",
+   //   video: "",
     },
     {
       title: "ООО АВАНГАРД",
