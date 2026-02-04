298e5f2cdd739aeaa0a1e778f751f21c3122d9fa
itibara99 <itibara99@gmail.com>
2026-02-04 11:40:19 +0300

    Updated CategoryPage.tsx

--- frontend/src/components/ceiling/CategoryPage.tsx
+++ frontend/src/components/ceiling/CategoryPage.tsx
@@ -230,8 +230,7 @@ const CategoryPage: React.FC<CategoryPageProps> = ({
           id: 'cor-5',
           name: 'Карниз B-1 однорядный',
           images: [
-            am1,
-            cornices
+            
           ],
           price: 'от 380 ₽/м',
           description: 'Угловой карниз для сложных конфигураций',
