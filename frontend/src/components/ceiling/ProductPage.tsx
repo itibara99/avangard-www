815a3219256d34de4bd9c907b2696ac38689c65f
itibara99 <itibara99@gmail.com>
2026-02-04 16:49:08 +0300

    Updated ProductPage.tsx

--- frontend/src/components/ceiling/ProductPage.tsx
+++ frontend/src/components/ceiling/ProductPage.tsx
@@ -367,7 +367,7 @@ const ProductPage: React.FC<ProductPageProps> = ({ productId, onBack, onOrderCli
       'cont-2': {
         name: 'Заглушки торцевые ПК-14',
         images: [
-          furnitures,
+          ,
           ''
         ],
         price: '45 ₽/шт',
