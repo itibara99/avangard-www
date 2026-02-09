39a2c776fb2d2e3f394bfb79ede4a19b67d249bf
itibara99 <itibara99@gmail.com>
2026-02-09 13:02:42 +0300

    Updated ProductPage.tsx

--- frontend/src/components/ceiling/ProductPage.tsx
+++ frontend/src/components/ceiling/ProductPage.tsx
@@ -359,7 +359,7 @@ const ProductPage: React.FC<ProductPageProps> = ({ productId, onBack, onOrderCli
         images: [
           b2_zagl,
         ],
-        price: '45 ₽/шт',
+        price: '300 ₽/комплект',
         description: 'TL-вставка для карнизов',
         fullDescription: 'Закрывает технический зазор профиля и предает эстетический вид.',
         specifications: {
