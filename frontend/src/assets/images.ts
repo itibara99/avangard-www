d92cd44dcbd8a8142d35ee0f999202a003a09795
itibara99 <itibara99@gmail.com>
2026-02-10 14:56:50 +0300

    Updated AvangardPage.tsx

--- frontend/src/assets/images.ts
+++ frontend/src/assets/images.ts
@@ -1,37 +1,35 @@
-import placeholder from './b2 zagl.jpg';
-
-const logo = placeholder;
-const bp40 = placeholder;
-const bp40_ch = placeholder;
-const stenvoy = placeholder;
-const sten_1 = placeholder;
-const sten_2 = placeholder;
-const sten_3 = placeholder;
-const razdelitel = placeholder;
-const razdelitel_ch = placeholder;
-const universal = placeholder;
-const universal_ch = placeholder;
-const potoloch = placeholder;
-const potoloch_ch = placeholder;
-const z = placeholder;
-const z_ch = placeholder;
-const b2 = placeholder;
-const b2_ch = placeholder;
-const b2_zagl = placeholder;
-const pk14_ch = placeholder;
-const cornices = placeholder;
-const pk15 = placeholder;
-const pk15_ch = placeholder;
-const pk12 = placeholder;
-const pk12_ch = placeholder;
-const am1 = placeholder;
-const furnitures = placeholder;
-const light_lines = placeholder;
-const ceilingSlide = placeholder;
+import logo from './logo1.png';
+import bp40 from './bp40.jpg';
+import bp40_ch from './ch/bp40 ch.jpg';
+import stenvoy from './stenovoy.jpg';
+import sten_1 from './ch/sten 1 ch.jpg';
+import sten_2 from './ch/sten 2 ch.jpg';
+import sten_3 from './ch/sten 3 ch.jpg';
+import razdelitel from './razdelitel.jpg';
+import razdelitel_ch from './ch/razdelitel ch.jpg';
+import universal from './universal.jpg';
+import universal_ch from './ch/univers ch.jpg';
+import potoloch from './potoloch.jpg';
+import potoloch_ch from './ch/potoloch ch.jpg';
+import z from './z.jpg';
+import z_ch from './ch/z ch.jpg';
+import b2 from './b2.jpg';
+import b2_ch from './ch/b2 ch.jpg';
+import b2_zagl from './b2 zagl.jpg';
+import pk14 from './pk14.jpg';
+import pk14_ch from './ch/pk14 ch.jpg';
+import cornices from './cornices.jpg';
+import pk15 from './pk15.jpg';
+import pk15_ch from './ch/pk15 ch.jpg';
+import pk12 from './pk12.jpg';
+import pk12_ch from './ch/pk12 ch.jpg';
+import furnitures from './furnitures.jpg';
+import light_lines from './light-lines.jpg';
+import ceilingSlide from './ceiling.jpg';
 
 export {
     bp40, bp40_ch, stenvoy, sten_1, sten_2, sten_3, razdelitel,
     razdelitel_ch, universal, universal_ch, potoloch, potoloch_ch,
-    z, z_ch, b2, b2_ch, b2_zagl, pk14_ch, cornices, pk15, pk15_ch, pk12, pk12_ch,
-    am1, furnitures, logo, light_lines, ceilingSlide
+    z, z_ch, b2, b2_ch, b2_zagl, pk14, pk14_ch, cornices, pk15, pk15_ch, pk12, pk12_ch,
+    furnitures, logo, light_lines, ceilingSlide
 }
