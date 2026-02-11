20cc90e147c80ce4f85a0299997b5f9ef019a806
itibara99 <itibara99@gmail.com>
2026-02-11 15:24:13 +0300

    Updated images.ts

--- frontend/src/assets/images.ts
+++ frontend/src/assets/images.ts
@@ -7,6 +7,7 @@ import sten_2 from './ch/sten 2 ch.jpg';
 import sten_3 from './ch/sten 3 ch.jpg';
 import razdelitel from './razdelitel.jpg';
 import razdelitel_ch from './ch/razdelitel ch.jpg';
+import reklamny from './reklamny.jpg';
 import universal from './universal.jpg';
 import universal_ch from './ch/univers ch.jpg';
 import potoloch from './potoloch.jpg';
