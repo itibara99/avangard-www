e6fbf88eaec87f5e3a206cbca7ab08b408a53362
itibara99 <itibara99@gmail.com>
2026-02-23 19:28:18 +0300

    Updated AvangardPage.tsx

--- frontend/src/assets/images.ts
+++ frontend/src/assets/images.ts
@@ -1,42 +1,38 @@
-import pool from './pool.jpg';
+import logo from './logo.png';
+import bp40 from './bp40.jpg';
+import bp40_ch from './ch/bp40 ch.jpg';
+import stenvoy from './stenovoy.jpg';
+import sten_1 from './ch/sten 1 ch.jpg';
+import sten_2 from './ch/sten 2 ch.jpg';
+import sten_3 from './ch/sten 3 ch.jpg';
+import razdelitel from './razdelitel.jpg';
+import razdelitel_ch from './ch/razdelitel ch.jpg';
+import reklamny from './reklamny.jpg';
+import oknadveri from '../assets/okna-dveri.jpg';
+import porogi from '../assets/porogi.jpg';
+import universal from './universal.jpg';
+import universal_ch from './ch/univers ch.jpg';
+import potoloch from './potoloch.jpg';
+import potoloch_ch from './ch/potoloch ch.jpg';
+import z from './z.jpg';
+import z_ch from './ch/z ch.jpg';
+import b2 from './b2.jpg';
+import b2_ch from './ch/b2 ch.jpg';
+import b2_zagl from './b2 zagl.jpg';
+import pk14_ch from './ch/pk14 ch.jpg';
+import cornices from './cornices.jpg';
+import pk15 from './pk15.jpg';
+import pk15_ch from './ch/pk15 ch.jpg';
 import pk12 from './pk12.jpg';
-import pk14 from './pk14.jpg';
-import dush from './dush.jpg';
-
-const bp40 = pk12;
-const bp40_ch = pk12;
-const stenvoy = pk12;
-const sten_1 = pk12;
-const sten_2 = pk12;
-const sten_3 = pk12;
-const razdelitel = pk12;
-const razdelitel_ch = pk12;
-const reklamny = pk12;
-const oknadveri = pk12;
-const porogi = pk12;
-const universal = pk12;
-const universal_ch = pk12;
-const potoloch = pk12;
-const potoloch_ch = pk12;
-const z = pk12;
-const z_ch = pk12;
-const b2 = pk12;
-const b2_ch = pk12;
-const b2_zagl = pk12;
-const pk14_ch = pk14;
-const cornices = pk14;
-const pk15 = pk14;
-const pk15_ch = pk14;
-const pk12_ch = pk12;
-const am1 = pk12;
-const furnitures = pk12;
-const light_lines = pk12;
-const ceilingSlide = pool;
-const logo = pool;
+import pk12_ch from './ch/pk12 ch.jpg';
+import am1 from './AM1.jpg';
+import furnitures from './furnitures.jpg';
+import light_lines from './light-lines.jpg';
+import ceilingSlide from './ceiling.jpg';
 
 export {
     bp40, bp40_ch, stenvoy, sten_1, sten_2, sten_3, razdelitel,
     razdelitel_ch, reklamny, oknadveri, porogi, universal, universal_ch, potoloch, potoloch_ch,
     z, z_ch, b2, b2_ch, b2_zagl, pk14_ch, cornices, pk15, pk15_ch, pk12, pk12_ch,
-    am1, furnitures, light_lines, ceilingSlide, logo
+    am1, furnitures, logo, light_lines, ceilingSlide
 }
